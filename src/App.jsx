ef46f113cfa9b023f6b8dc5f239b7e5c2c10c065
alin <alinps8406@gmail.com>
2026-05-10 22:18:51 +0530

    update route path for admin login and link title in Landing component

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
             />
 
             <Route
-            path="/admin/login"
+            path="/admin_login"
             element={
               <AdminLogin />
             }
